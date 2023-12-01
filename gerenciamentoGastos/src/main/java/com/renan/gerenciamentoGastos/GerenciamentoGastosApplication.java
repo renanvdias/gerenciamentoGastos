814f899bc75b5e0c0c6f6fc046a8e0d737ffeda5
Renan Vitor <renanvitoramaral@gmail.com>
2023-12-01 15:53:39 -0300

    implementacao inicial
    implementação inicial do html e css da página principal

---
+++ gerenciamentoGastos/src/main/java/com/renan/gerenciamentoGastos/GerenciamentoGastosApplication.java
@@ -0,0 +1,13 @@
+package com.renan.gerenciamentoGastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoGastosApplication.class, args);
+	}
+
+}
---
+++ gerenciamentoGastos/src/test/java/com/renan/gerenciamentoGastos/GerenciamentoGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.gerenciamentoGastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
